5b6dd3a0329d3b68d30aecb36ff934360b8517d4
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-02-04 11:01:48 +0700

    Add tests for CheckboxGroup component

--- src/components/CheckboxGroup/CheckboxGroup.test.tsx
+++ src/components/CheckboxGroup/CheckboxGroup.test.tsx
@@ -1,1 +1,46 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, it } from 'vitest';
+import { CheckboxGroup } from './CheckboxGroup';
+import { Checkbox } from '../Checkbox';
 
+describe('Checkbox', async () => {
+	it('should render the checkboxes', () => {
+		render(
+			<CheckboxGroup label="Post types">
+				<Checkbox>Pages</Checkbox>
+				<Checkbox>Posts</Checkbox>
+			</CheckboxGroup>
+		);
+
+		expect(
+			screen.getByRole('group', { name: 'Post types' })
+		).toBeInTheDocument();
+		expect(screen.getByRole('checkbox', { name: 'Pages' })).toBeInTheDocument();
+		expect(screen.getByRole('checkbox', { name: 'Posts' })).toBeInTheDocument();
+	});
+
+	it('should disable the checkboxes', () => {
+		render(
+			<CheckboxGroup label="Post types" isDisabled>
+				<Checkbox>Pages</Checkbox>
+				<Checkbox>Posts</Checkbox>
+			</CheckboxGroup>
+		);
+
+		expect(screen.getByRole('group', { name: 'Post types' })).toBeDisabled();
+		expect(screen.getByRole('checkbox', { name: 'Pages' })).toBeDisabled();
+		expect(screen.getByRole('checkbox', { name: 'Posts' })).toBeDisabled();
+	});
+
+	it('should disable the checkboxes', () => {
+		render(
+			<CheckboxGroup label="Post types" isInvalid>
+				<Checkbox>Pages</Checkbox>
+				<Checkbox>Posts</Checkbox>
+			</CheckboxGroup>
+		);
+
+		expect(screen.getByRole('checkbox', { name: 'Pages' })).toBeInvalid();
+		expect(screen.getByRole('checkbox', { name: 'Posts' })).toBeInvalid();
+	});
+});
