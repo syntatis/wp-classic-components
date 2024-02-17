d7ed188f4cf299598cb37d5f676cd2fb6f0522ed
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-02-17 16:29:06 +0700

    Add @syntatis/types package

--- packages/types/extensions.d.ts
+++ packages/types/extensions.d.ts
@@ -1,7 +1,23 @@
-declare module '*.svg';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare module '*.png';
 declare module '*.webp';
 declare module '*.jpg';
 declare module '*.gif';
 declare module '*.webp';
-declare module '*.css';
+declare module '*.svg' {
+	/**
+	 * Use `any` to avoid conflicts with
+	 * `@svgr/webpack` plugin or
+	 * `babel-plugin-inline-react-svg` plugin.
+	 */
+	const content: any;
+
+	export default content;
+}
+declare module '*.scss' {
+	interface IClassNames {
+		[className: string]: string;
+	}
+	const classNames: IClassNames;
+	export = classNames;
+}
