a31458868e7cd7930091386ac7334935a5f940da
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-02-17 22:47:12 +0700

    Update ESLing configs

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,37 +2,36 @@ import { StorybookConfig } from '@storybook/react-vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 const config: StorybookConfig = {
-  stories: [
-    '../packages/wp-classic-components/src/**/*.stories.@(ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-a11y',
-    '@storybook/addon-essentials',
-    '@storybook/addon-links',
-    '@storybook/addon-styling',
-  ],
-  core: {},
-  docs: {
-    autodocs: 'tag',
+	stories: ['../packages/wp-classic-components/src/**/*.stories.@(ts|tsx)'],
+	addons: [
+		'@storybook/addon-a11y',
+		'@storybook/addon-essentials',
+		'@storybook/addon-links',
+		'@storybook/addon-styling',
+	],
+	core: {},
+	docs: {
+		autodocs: 'tag',
 	},
-  framework: {
-    name: '@storybook/react-vite',
-    options: {}
-  },
-  typescript: {
-    reactDocgen: 'react-docgen-typescript',
-    reactDocgenTypescriptOptions: {
-      compilerOptions: {
-        allowSyntheticDefaultImports: false,
-        esModuleInterop: false,
-      },
-      propFilter: () => true,
-    },
+	framework: {
+		name: '@storybook/react-vite',
+		options: {},
+	},
+	typescript: {
+		reactDocgen: 'react-docgen-typescript',
+		reactDocgenTypescriptOptions: {
+			compilerOptions: {
+				allowSyntheticDefaultImports: false,
+				esModuleInterop: false,
+			},
+			propFilter: () => true,
+		},
 	},
 	viteFinal: (config) => {
 		config.plugins?.push(tsConfigPaths());
+
 		return config;
-	}
+	},
 };
 
 export default config;
--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,62 +1,62 @@
-import React, { useEffect } from 'react';
 import { Decorator, Preview } from '@storybook/react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import React, { useEffect } from 'react';
 import './preview.scss';
 
 const DEFAULT_THEME = 'light' as const;
-
 const ThemeDecorator: Decorator = (Story, context) => {
 	const id = context.id;
-  const theme = context.globals?.theme || DEFAULT_THEME;
-  const viewMode = context?.viewMode;
+	const theme = context.globals?.theme || DEFAULT_THEME;
+	const viewMode = context?.viewMode;
 
-  useEffect(() => {
-    if (viewMode === 'docs') {
-      const selector = `#anchor--${id} .docs-story`;
-      document.querySelectorAll(selector).forEach((el) => {
-        if (el instanceof HTMLElement) {
-          el.classList.add('__wp-core-body__');
-        }
-      });
-    }
+	useEffect(() => {
+		if (viewMode === 'docs') {
+			const selector = `#anchor--${id} .docs-story`;
+
+			document.querySelectorAll(selector).forEach((el) => {
+				if (el instanceof HTMLElement) {
+					el.classList.add('__wp-core-body__');
+				}
+			});
+		}
 	}, [theme, viewMode, id]);
 
 	return (
 		<div className="wp-core-ui">
-			<div style={{ maxWidth: "50vw", width: "100%" }}>
+			<div style={{ maxWidth: '50vw', width: '100%' }}>
 				<Story {...context} />
 			</div>
 		</div>
 	);
-}
-
+};
 const preview: Preview = {
-  decorators: [ThemeDecorator],
-  globalTypes: {
-    theme: {
-      defaultValue: DEFAULT_THEME,
-    },
-  },
+	decorators: [ThemeDecorator],
+	globalTypes: {
+		theme: {
+			defaultValue: DEFAULT_THEME,
+		},
+	},
 	parameters: {
 		actions: {
 			argTypesRegex: '^on[A-Z].*',
 		},
-    options: {
-      storySort: {
-        method: 'alphabetical',
-      },
-    },
-    controls: {
-      hideNoControlsWarning: true,
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+		options: {
+			storySort: {
+				method: 'alphabetical',
+			},
+		},
+		controls: {
+			hideNoControlsWarning: true,
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/,
+			},
 			expanded: true,
 			sort: 'requiredFirst',
-    },
-    backgrounds: {
-      disable: true,
-    },
+		},
+		backgrounds: {
+			disable: true,
+		},
 	},
 };
 
