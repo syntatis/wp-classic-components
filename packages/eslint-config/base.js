2fed14bb58a431956ad48db27f3b587e652d0b82
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-02-18 11:44:36 +0700

    Update ESLint configuration files

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -6,30 +6,32 @@ module.exports = {
 	},
 	extends: [
 		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
 		'plugin:import/recommended',
-		'plugin:import/typescript',
-		'plugin:react-hooks/recommended',
-		'plugin:react/recommended',
-		'plugin:react/jsx-runtime',
 		'plugin:prettier/recommended',
 		'plugin:perfectionist/recommended-natural',
 	],
 	overrides: [
 		{
-			files: ['*.config.{js,jsx,ts,tsx,mts}', '*.d.{js,jsx,ts,tsx}'],
+			files: [
+				'*.config.{js,mjs,cjs}',
+				'*.d.{js,mjs,cjs}',
+				'*.spec.{js,mjs,cjs}',
+				'*.test.{js,mjs,cjs}',
+			],
 			rules: {
 				'import/no-default-export': 'off',
-			},
-		},
-		{
-			files: ['*.test.{js,jsx,ts,tsx,mts}'],
-			rules: {
-				'@typescript-eslint/ban-ts-comment': 'off',
+				'import/no-named-as-default': 'off',
 			},
 		},
 	],
-	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			globalReturn: false,
+			jsx: true,
+		},
+		ecmaVersion: 2020,
+		sourceType: `module`,
+	},
 	plugins: ['@stylistic'],
 	rules: {
 		'@stylistic/max-len': [
@@ -88,7 +90,6 @@ module.exports = {
 				prev: 'case',
 			},
 		],
-		'@typescript-eslint/consistent-type-definitions': ['error', 'interface'],
 		'import/newline-after-import': [
 			'error',
 			{
@@ -122,21 +123,10 @@ module.exports = {
 				useTabs: true,
 			},
 		],
-		'react/function-component-definition': [
-			2,
-			{
-				namedComponents: 'arrow-function',
-			},
-		],
-		'react/prop-types': 'off',
 	},
 	settings: {
 		'import/resolver': {
 			node: true,
-			typescript: true,
-		},
-		react: {
-			version: 'detect',
 		},
 	},
 };
