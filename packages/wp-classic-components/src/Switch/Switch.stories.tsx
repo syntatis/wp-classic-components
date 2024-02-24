a4dbd7f41ef0bef32f7256f046675061b249b4ca
Thoriq Firdaus <2067467+tfirdaus@users.noreply.github.com>
2024-02-24 16:29:18 +0700

    Update Switch component

--- packages/wp-classic-components/src/Switch/Switch.stories.tsx
+++ packages/wp-classic-components/src/Switch/Switch.stories.tsx
@@ -2,12 +2,30 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Switch } from './Switch';
 
 const meta: Meta<typeof Switch> = {
+	argTypes: {
+		children: {
+			control: 'text',
+		},
+		description: {
+			control: 'text',
+		},
+	},
 	args: {
 		children: 'Show in REST API',
 	},
 	component: Switch,
 	parameters: {
-		controls: {},
+		controls: {
+			include: [
+				'children',
+				'description',
+				'isDisabled',
+				'isReadOnly',
+				'isSelected',
+				'onPress',
+				'onChange',
+			],
+		},
 	},
 	tags: ['autodocs'],
 	title: 'Components/Switch',
@@ -16,5 +34,45 @@ const meta: Meta<typeof Switch> = {
 type Story = StoryObj<typeof Switch>;
 
 export const Default: Story = {};
+
+export const Disabled: Story = {
+	args: {
+		isDisabled: true,
+		isSelected: true,
+	},
+	parameters: {
+		controls: {
+			exclude: ['isDisabled'],
+		},
+	},
+};
+
+export const Selected: Story = {
+	args: {
+		defaultSelected: true,
+	},
+	name: 'Selected (default)',
+};
+
+export const SelectedControlled: Story = {
+	args: {
+		isSelected: true,
+	},
+	name: 'Selected (controlled)',
+};
+
+export const ReadOnly: Story = {
+	args: {
+		defaultSelected: true,
+		isReadOnly: true,
+	},
+	name: 'ReadOnly',
+};
+
+export const WithDescription: Story = {
+	args: {
+		description: 'When enabled it will show post type in REST API.',
+	},
+};
 
 export default meta;
--- packages/wp-classic-components/src/Switch/Switch.tsx
+++ packages/wp-classic-components/src/Switch/Switch.tsx
@@ -1,85 +1,120 @@
 import { useProps } from '@/hooks';
 import { GlobalProps } from '@/types';
-import { forwardRef } from 'react';
+import { ReactNode, forwardRef } from 'react';
 import {
 	AriaSwitchProps,
 	VisuallyHidden,
 	useFocusRing,
+	useId,
 	useObjectRef,
 	useSwitch,
 } from 'react-aria';
 import { useToggleState } from 'react-stately';
 import * as classes from './Switch.module.scss';
 
-interface SwitchProps extends GlobalProps, AriaSwitchProps {}
+interface SwitchProps extends GlobalProps, AriaSwitchProps {
+	/**
+	 * A description for the field. Provides a hint such for what the input is more about.
+	 */
+	description?: ReactNode;
+}
 
 export const Switch = forwardRef<HTMLInputElement, SwitchProps>(
 	(props, forwardedRef) => {
-		const { children } = props;
+		const { children, description } = props;
 		const ref = useObjectRef(forwardedRef);
 		const state = useToggleState(props);
 		const { clsx, componentProps, rootProps } = useProps('Switch', props);
-		const { inputProps, labelProps } = useSwitch(componentProps, state, ref);
+		const { inputProps, isDisabled, labelProps } = useSwitch(
+			componentProps,
+			state,
+			ref
+		);
 		const { focusProps, isFocusVisible } = useFocusRing();
+		// eslint-disable-next-line react-hooks/rules-of-hooks
+		const descriptionId = description ? useId() : undefined;
 
 		return (
-			<label
-				{...rootProps({
-					classNames: [
-						classes.root,
-						{
-							[classes.isSelected]: state.isSelected,
-						},
-					],
-				})}
-			>
-				<VisuallyHidden>
-					<input {...inputProps} {...focusProps} ref={ref} />
-				</VisuallyHidden>
-				<svg aria-hidden="true" height={28} width={44}>
-					<rect
-						className={clsx({
-							classNames: [classes.track],
-						})}
-						height={20}
-						rx={10}
-						width={36}
-						x={4}
-						y={4}
-					/>
-					<circle
-						className={clsx({
-							classNames: [classes.thumb],
-						})}
-						cx={state.isSelected ? 30 : 14}
-						cy={14}
-						r={8}
-					/>
-					{isFocusVisible && (
-						<rect
-							className={clsx({
-								classNames: [classes.focusRing],
-								prefixedNames: 'focusRing',
-							})}
-							fill="none"
-							height={26}
-							rx={14}
-							strokeWidth={2}
-							width={42}
-							x={1}
-							y={1}
-						/>
-					)}
-				</svg>
-				<span
-					{...labelProps}
-					className={clsx({
-						classNames: [classes.label],
+			<>
+				<label
+					{...rootProps({
+						classNames: [
+							classes.root,
+							{
+								[classes.isDisabled]: isDisabled,
+								[classes.isSelected]: state.isSelected,
+							},
+						],
 					})}
+					{...labelProps}
+					aria-describedby={descriptionId}
 				>
-					{children}
-				</span>
-			</label>
+					<VisuallyHidden>
+						<input {...inputProps} {...focusProps} ref={ref} />
+					</VisuallyHidden>
+					<div
+						className={clsx({
+							classNames: [classes.input],
+							prefixedNames: 'input',
+						})}
+					>
+						<svg aria-hidden="true" height={28} width={44}>
+							<rect
+								className={clsx({
+									classNames: [classes.track],
+								})}
+								height={20}
+								rx={10}
+								width={36}
+								x={4}
+								y={4}
+							/>
+							<circle
+								className={clsx({
+									classNames: [classes.thumb],
+								})}
+								cx={state.isSelected ? 30 : 14}
+								cy={14}
+								r={8}
+							/>
+							{isFocusVisible && (
+								<rect
+									className={clsx({
+										classNames: [classes.focusRing],
+										prefixedNames: 'focusRing',
+									})}
+									fill="none"
+									height={26}
+									rx={14}
+									strokeWidth={2}
+									width={42}
+									x={1}
+									y={1}
+								/>
+							)}
+						</svg>
+						<span
+							className={clsx({
+								classNames: [classes.label],
+								prefixedNames: 'label',
+							})}
+						>
+							{children}
+						</span>
+					</div>
+				</label>
+				{description && (
+					<p
+						className={clsx({
+							classNames: [classes.label, 'description'],
+							prefixedNames: 'description',
+						})}
+						id={descriptionId}
+					>
+						{description}
+					</p>
+				)}
+			</>
 		);
 	}
 );
